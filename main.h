2fce55d66dcf1945f29ad0909e0ca207c82d784f
Immanoel1885 <111055109+Immanoel1885@users.noreply.github.com>
2022-10-17 12:05:01 -0700

    add u, o, x, X prototypes

--- main.h
+++ main.h
@@ -28,6 +28,10 @@ int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
 int print_binary(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
