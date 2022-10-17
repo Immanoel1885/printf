f1b7307b6fea8ca2687b081945620722c2095637
Immanoel1885 <iamikegwuatu@gmail.com>
2022-10-17 01:36:44 -0700

    merging repo

--- main.h
+++ main.h
@@ -1,21 +1,24 @@
 #ifndef PRINT_F
 #define PRINT_F
 
+#include <stdio.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
 /**
-* struct convert - drfines a structure for symbols and functios
-* @sym: the operator
-* @f: the function associated
-*/
+ * struct convert - defines a structure for symbols and functions
+ *
+ *
+ * @sym: the operator
+ * @f: the fynction associated
+ */
 struct convert
 {
-  char *sym;
-  int (*f)(va_list);
+	char *sym;
+	int (*f)(va_list);
 };
-typedef struct convert coner_t;
+typedef struct convert conver_t;
 
 /*main functions*/
 int parser(const char *format, conver_t f_list[], va_list arg_list);
@@ -26,20 +29,4 @@ int print_string(va_list);
 int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
-
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
-
-
-#endif
+int print_binary(va_list)
