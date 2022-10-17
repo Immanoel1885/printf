d522b0cc9627d29090c7d385d441b2f5440ed288
Judeitopa <109626709+Judeitopa@users.noreply.github.com>
2022-10-17 19:28:07 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -11,17 +11,17 @@
 int _printf(const char *format, ...)
 {
 int printed_chars;
-  conver_t f_list[] = {
-    {"c", print_char},
-    {"s", print_string},
-    {"%", print_percent},
-    {NULL, NULL}
-  };
-  va_list arg_list;
-  if (format == NULL)
-    return (-1);
-  va_start(arg_list, format);
-  printed_chars = parser(format, f_list, arg_list);
-  va_end(arg_list);
-  return (printed_chars);
+conver_t f_list[] = {
+{"c", print_char},
+{"s", print_string},
+{"%", print_percent},
+{NULL, NULL}
+};
+va_list arg_list;
+if (format == NULL)
+return (-1);
+va_start(arg_list, format);
+printed_chars = parser(format, f_list, arg_list);
+va_end(arg_list);
+return (printed_chars);
 }
