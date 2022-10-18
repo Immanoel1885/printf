8e66b8c7b1be86911e1095715368f964dfd81cfe
Immanoel1885 <111055109+Immanoel1885@users.noreply.github.com>
2022-10-18 09:07:23 -0500

    resolve betty line spacing errors

--- _print_String.c
+++ _print_String.c
@@ -2,38 +2,38 @@
 
 /**
  * print_String - prints out a string, with non printable chracters printed, thus
- * \x, followed by ASCII value in hexadecimal in uppercase, always 2 characters
+ * followed by ASCII value in hexadecimal in uppercase, always 2 characters
  * @list: the list of arguments
  * Return: the number of printed characters
  */
 
 int print_String(va_list list)
 {
-  int i;
-  char *str;
-  int count = 0;
+int i;
+char *str;
+int count = 0;
   
-  str = va_arg(list, char *);
+str = va_arg(list, char *);
   
-  if (str == NULL)
-  {
-    str = "(null)";
-  }
-  for (i = 0; str[i] != '\0'; i++)
-  {
-    if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64) || (str[i] >= 91 && str[i] <= 96) || (str[i] >= 123 && str[i] <= 126))
-        {
-          _write_char('\\');
-          _write_char('x');
-          count += 2;
-          count += _printf("%X", str[i]);
-        }
-    else
-        {
-          _write_char(str[i]);
-          count++;
-        }
-  }
-  return (count);
+if (str == NULL)
+{
+str = "(null)";
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((str[i] >= 32 && str[i] <= 47) || (str[i] >= 58 && str[i] <= 64) || (str[i] >= 91 && str[i] <= 96) || (str[i] >= 123 && str[i] <= 126))
+{
+_write_char('\\');
+_write_char('x');
+count += 2;
+count += _printf("%X", str[i]);
+}
+else
+{
+_write_char(str[i]);
+count++;
+}
+}
+return (count);
 }
 
